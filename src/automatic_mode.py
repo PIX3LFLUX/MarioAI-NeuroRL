77aad618cb7ef11abbf04f685f5cbbd395c7a0b1
Elias Fick <128058801+Chainsaw1860@users.noreply.github.com>
2023-10-31 19:57:42 +0900

    Update automatic_mode.py

--- src/automatic_mode.py
+++ src/automatic_mode.py
@@ -18,43 +18,43 @@ automatic_mode = "True"
 # Choose right level and model for next run	
 if last_level == "" and last_model == "":
 	level = "SuperMarioBros-1-1-v0" 
-	model = "model1"
+	model = "../models/model1"
 elif last_level == "SuperMarioBros-1-1-v0" and last_model == "model1":
 	level = "SuperMarioBros-1-2-v0"
-	model = "1000it"
+	model = "../models/1000it"
 elif last_level == "SuperMarioBros-1-2-v0" and last_model == "1000it":
 	level = "SuperMarioBros-1-3-v0"
-	model = "ea1000it"
+	model = "../models/ea1000it"
 elif last_level == "SuperMarioBros-1-3-v0" and last_model == "ea1000it":
 	level = "SuperMarioBros-1-1-v0"
-	model = "model2"
+	model = "../models/model2"
 elif last_level == "SuperMarioBros-1-1-v0" and last_model == "model2":
 	level = "SuperMarioBros-1-2-v0"
-	model = "2000it"
+	model = "../models/2000it"
 elif last_level == "SuperMarioBros-1-2-v0" and last_model == "2000it":
 	level = "SuperMarioBros-1-3-v0"
-	model = "ea2000it"
+	model = "../models/ea2000it"
 elif last_level == "SuperMarioBros-1-3-v0" and last_model == "ea2000it":
 	level = "SuperMarioBros-1-1-v0"
-	model = "model3"
+	model = "../models/model3"
 elif last_level == "SuperMarioBros-1-1-v0" and last_model == "model3":
 	level = "SuperMarioBros-1-2-v0"
-	model = "8000it"
+	model = "../models/8000it"
 elif last_level == "SuperMarioBros-1-2-v0" and last_model == "8000it":
 	level = "SuperMarioBros-1-3-v0"
-	model = "ea8000it"
+	model = "../models/ea8000it"
 elif last_level == "SuperMarioBros-1-3-v0" and last_model == "ea8000it":
 	level = "SuperMarioBros-1-1-v0"
-	model = "model4"
+	model = "../models/model4"
 elif last_level == "SuperMarioBros-1-1-v0" and last_model == "model4":
 	level = "SuperMarioBros-1-2-v0"
-	model = "austrainiert"
+	model = "../models/austrainiert"
 elif last_level == "SuperMarioBros-1-2-v0" and last_model == "austrainiert":
 	level = "SuperMarioBros-1-3-v0"
-	model = "eaaustrainiert"
+	model = "../models/eaaustrainiert"
 elif last_level == "SuperMarioBros-1-3-v0" and last_model == "eaaustrainiert":
 	level = "SuperMarioBros-1-1-v0"
-	model = "model1"
+	model = "../models/model1"
 	
 # Build the command to execute the Test_models.py script
 command = ["python", "test_models.py", level, model, automatic_mode]
